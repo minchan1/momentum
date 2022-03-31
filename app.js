c6ee422525ab38948897f4b7946fa0fe60ec0e8c
minchan1 <alflscks@naver.com>
2022-03-31 16:38:42 +0900

    0331

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-alert("hi");
+// alert("hi");
+
+// const toBuy = ["abc","def","asd"];
+
+// console.log(toBuy);
+// console.log(toBuy[2]);
+// toBuy.push("lkj");
+// console.log(toBuy);
+// toBuy[2] = "eee";
+// console.log(toBuy);
+
+// const calculator = {
+//     plus:function(a,b) {
+//         return a+b;
+//     },
+//     minus:function(a,b) {
+//         console.log(a-b);
+//     },
+//     divide:function(a,b) {
+//         console.log(a/b);
+//     },
+//     power:function(a,b) {
+//         console.log(a**b);
+//     }
+// };
+// const answer = calculator.plus(2,3);
+// console.log(Number('123asd'));
+// console.log(typeof parseInt('123asd'));
+// console.log(typeof 34);
+
+const example = document.getElementById("temp");
+
+// example.innerText = "abc";
+console.log(example.className);
